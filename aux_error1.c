36178beea951cccb587ad4b3020c0fb0a62cc8cd
abdelhamed atef <abdelhamedatef2@gmail.com>
2023-04-28 14:40:12 -0700

    update code

--- aux_error1.c
+++ aux_error1.c
@@ -1,55 +1,55 @@
 #include "main.h"
 
 /**
- * strcat_cd - function that concatenates the message for cd error
+ * concat_err_cd - function that concatenates the message for cd error
  *
- * @datash: data relevant (directory)
+ * @data: data relevant (directory)
  * @msg: message to print
- * @error: output message
- * @ver_str: counter lines
+ * @err_msg: output message
+ * @line_count: counter lines
  * Return: error message
  */
-char *strcat_cd(data_shell *datash, char *msg, char *error, char *ver_str)
+char *concat_err_cd(data_shell *data, char *msg, char *err_msg, char *line_count)
 {
 	char *illegal_flag;
 
-	_strcpy(error, datash->av[0]);
-	_strcat(error, ": ");
-	_strcat(error, ver_str);
-	_strcat(error, ": ");
-	_strcat(error, datash->args[0]);
-	_strcat(error, msg);
-	if (datash->args[1][0] == '-')
+	_strcpy(err_msg, data->av[0]);
+	_strcat(err_msg, ": ");
+	_strcat(err_msg, line_count);
+	_strcat(err_msg, ": ");
+	_strcat(err_msg, data->args[0]);
+	_strcat(err_msg, msg);
+	if (data->args[1][0] == '-')
 	{
 		illegal_flag = malloc(3);
 		illegal_flag[0] = '-';
-		illegal_flag[1] = datash->args[1][1];
+		illegal_flag[1] = data->args[1][1];
 		illegal_flag[2] = '\0';
-		_strcat(error, illegal_flag);
+		_strcat(err_msg, illegal_flag);
 		free(illegal_flag);
 	}
 	else
 	{
-		_strcat(error, datash->args[1]);
+		_strcat(err_msg, data->args[1]);
 	}
 
-	_strcat(error, "\n");
-	_strcat(error, "\0");
-	return (error);
+	_strcat(err_msg, "\n");
+	_strcat(err_msg, "\0");
+	return (err_msg);
 }
 
 /**
- * error_get_cd - error message for cd command in get_cd
- * @datash: data relevant (directory)
+ * error_msg_cd - error message for cd command in get_cd
+ * @data: data relevant (directory)
  * Return: Error message
  */
-char *error_get_cd(data_shell *datash)
+char *error_msg_cd(data_shell *data)
 {
-	int length, len_id;
-	char *error, *ver_str, *msg;
+	int len, len_id;
+	char *err_msg, *line_count, *msg;
 
-	ver_str = aux_itoa(datash->counter);
-	if (datash->args[1][0] == '-')
+	line_count = aux_itoa(data->counter);
+	if (data->args[1][0] == '-')
 	{
 		msg = ": Illegal option ";
 		len_id = 2;
@@ -57,88 +57,88 @@ char *error_get_cd(data_shell *datash)
 	else
 	{
 		msg = ": can't cd to ";
-		len_id = _strlen(datash->args[1]);
+		len_id = _strlen(data->args[1]);
 	}
 
-	length = _strlen(datash->av[0]) + _strlen(datash->args[0]);
-	length += _strlen(ver_str) + _strlen(msg) + len_id + 5;
-	error = malloc(sizeof(char) * (length + 1));
+	len = _strlen(data->av[0]) + _strlen(data->args[0]);
+	len += _strlen(line_count) + _strlen(msg) + len_id + 5;
+	err_msg = malloc(sizeof(char) * (len + 1));
 
-	if (error == 0)
+	if (err_msg == 0)
 	{
-		free(ver_str);
+		free(line_count);
 		return (NULL);
 	}
 
-	error = strcat_cd(datash, msg, error, ver_str);
+	err_msg = concat_err_cd(data, msg, err_msg, line_count);
 
-	free(ver_str);
+	free(line_count);
 
-	return (error);
+	return (err_msg);
 }
 
 /**
  * error_not_found - generic error message for command not found
- * @datash: data relevant (counter, arguments)
+ * @data: data relevant (counter, arguments)
  * Return: Error message
  */
-char *error_not_found(data_shell *datash)
+char *error_not_found(data_shell *data)
 {
-	int length;
-	char *error;
-	char *ver_str;
+	int len;
+	char *err_msg;
+	char *line_count;
 
-	ver_str = aux_itoa(datash->counter);
-	length = _strlen(datash->av[0]) + _strlen(ver_str);
-	length += _strlen(datash->args[0]) + 16;
-	error = malloc(sizeof(char) * (length + 1));
-	if (error == 0)
+	line_count = aux_itoa(data->counter);
+	len = _strlen(data->av[0]) + _strlen(line_count);
+	len += _strlen(data->args[0]) + 16;
+	err_msg = malloc(sizeof(char) * (len + 1));
+	if (err_msg == 0)
 	{
-		free(error);
-		free(ver_str);
+		free(err_msg);
+		free(line_count);
 		return (NULL);
 	}
-	_strcpy(error, datash->av[0]);
-	_strcat(error, ": ");
-	_strcat(error, ver_str);
-	_strcat(error, ": ");
-	_strcat(error, datash->args[0]);
-	_strcat(error, ": not found\n");
-	_strcat(error, "\0");
-	free(ver_str);
-	return (error);
+	_strcpy(err_msg, data->av[0]);
+	_strcat(err_msg, ": ");
+	_strcat(err_msg, line_count);
+	_strcat(err_msg, ": ");
+	_strcat(err_msg, data->args[0]);
+	_strcat(err_msg, ": not found\n");
+	_strcat(err_msg, "\0");
+	free(line_count);
+	return (err_msg);
 }
 
 /**
  * error_exit_shell - generic error message for exit in get_exit
- * @datash: data relevant (counter, arguments)
+ * @data: data relevant (counter, arguments)
  *
  * Return: Error message
  */
-char *error_exit_shell(data_shell *datash)
+char *error_exit_shell(data_shell *data)
 {
-	int length;
-	char *error;
-	char *ver_str;
+	int len;
+	char *err_msg;
+	char *line_count;
 
-	ver_str = aux_itoa(datash->counter);
-	length = _strlen(datash->av[0]) + _strlen(ver_str);
-	length += _strlen(datash->args[0]) + _strlen(datash->args[1]) + 23;
-	error = malloc(sizeof(char) * (length + 1));
-	if (error == 0)
+	line_count = aux_itoa(data->counter);
+	len = _strlen(data->av[0]) + _strlen(line_count);
+	len += _strlen(data->args[0]) + _strlen(data->args[1]) + 23;
+	err_msg = malloc(sizeof(char) * (len + 1));
+	if (err_msg == 0)
 	{
-		free(ver_str);
+		free(line_count);
 		return (NULL);
 	}
-	_strcpy(error, datash->av[0]);
-	_strcat(error, ": ");
-	_strcat(error, ver_str);
-	_strcat(error, ": ");
-	_strcat(error, datash->args[0]);
-	_strcat(error, ": Illegal number: ");
-	_strcat(error, datash->args[1]);
-	_strcat(error, "\n\0");
-	free(ver_str);
+	_strcpy(err_msg, data->av[0]);
+	_strcat(err_msg, ": ");
+	_strcat(err_msg, line_count);
+	_strcat(err_msg, ": ");
+	_strcat(err_msg, data->args[0]);
+	_strcat(err_msg, ": Illegal number: ");
+	_strcat(err_msg, data->args[1]);
+	_strcat(err_msg, "\n\0");
+	free(line_count);
 
-	return (error);
+	return (err_msg);
 }
